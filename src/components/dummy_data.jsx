febf99a7223312bf5df91dea2924b2be35225259
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-17 17:58:54 +0330

    lists of data added to DUMMY_DATA file

---
+++ src/components/dummy_data.jsx
@@ -0,0 +1,35 @@
+const DUMMY_DATA = [
+    {
+        id: 1,
+        cost: "500$",
+        link: "https://images.hindustantimes.com/tech/htmobile4/P35953/heroimage/143993-v4-apple-iphone-14-mobile-phone-large-1.jpg"
+    }, {
+        id: 2,
+        cost: "1280$",
+        link: "https://t-mobile.scene7.com/is/image/Tmusprod/fg-Samsung_Galaxy-Flip_5_Hero_nologo?ts=1690309549049&$pngtransparent$&dpr=off"
+    }, {
+        id: 3,
+        cost: "740$",
+        link: "https://i02.appmifile.com/998_operator_in/12/08/2021/3c3be7deaa0112fa9c5c4c0c3e1da35d.png"
+    }, {
+        id: 4,
+        cost: "628$",
+        link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmiAKtiWFbFuOGFav4liY881ikLKeHK96rlw&usqp=CAU"
+    }, {
+        id: 5,
+        cost: "3282$",
+        link: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWBrzy?ver=85d4&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true"
+    }, {
+        id: 6,
+        cost: "60$",
+        link: "https://cdn.thewirecutter.com/wp-content/media/2023/06/laptops-2048px-5607.jpg?auto=webp&quality=75&crop=1.91:1&width=1200"
+    }, {
+        id: 7,
+        cost: "5$",
+        link: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4OXzi?ver=3a58"
+    }, {
+        id: 8,
+        cost: "120$",
+        link: "https://cdn.mos.cms.futurecdn.net/ytKr6L3ZTP4Pf2acEpzpMM.jpeg"
+    }
+]
