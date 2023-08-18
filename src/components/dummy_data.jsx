db13b4868e7176f01cc70456e1f4154506318d29
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-18 14:55:34 +0330

    some links edited and add model key

--- src/components/dummy_data.jsx
+++ src/components/dummy_data.jsx
@@ -1,34 +1,42 @@
 const DUMMY_DATA = [
     {
         id: 1,
+        model : "iphone 11 pro",
         cost: "500$",
-        link: "https://images.hindustantimes.com/tech/htmobile4/P35953/heroimage/143993-v4-apple-iphone-14-mobile-phone-large-1.jpg"
+        link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo_DxbDLGl62hYIt4HsjpS0KOqVc2UDErHMw&usqp=CAU"
     }, {
         id: 2,
+        model : "samsung M53",
         cost: "1280$",
-        link: "https://t-mobile.scene7.com/is/image/Tmusprod/fg-Samsung_Galaxy-Flip_5_Hero_nologo?ts=1690309549049&$pngtransparent$&dpr=off"
+        link: "https://images.samsung.com/is/image/samsung/p6pim/in/sm-m536bzbfinu/gallery/in-galaxy-m53-5g-sm-m536-421705-sm-m536bzbfinu-532181615?$650_519_PNG$"
     }, {
         id: 3,
+        model : "samsung Z flip5",
         cost: "740$",
-        link: "https://i02.appmifile.com/998_operator_in/12/08/2021/3c3be7deaa0112fa9c5c4c0c3e1da35d.png"
+        link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZQTmTEoOZcidZf7vMD6owQAj8KIB-Ir4TWJaWn2TNcKNf6a3bHTFnu1UvkA4vCVCrQsQ&usqp=CAU"
     }, {
         id: 4,
+        model : "samsung X23",
         cost: "628$",
         link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmiAKtiWFbFuOGFav4liY881ikLKeHK96rlw&usqp=CAU"
     }, {
         id: 5,
+        model : "surface pro 10",
         cost: "3282$",
         link: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWBrzy?ver=85d4&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true"
     }, {
         id: 6,
+        model : "HP Envy",
         cost: "60$",
         link: "https://cdn.thewirecutter.com/wp-content/media/2023/06/laptops-2048px-5607.jpg?auto=webp&quality=75&crop=1.91:1&width=1200"
     }, {
         id: 7,
+        model : "surfac pro 9",
         cost: "5$",
         link: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4OXzi?ver=3a58"
     }, {
         id: 8,
+        model : "macbook pro",
         cost: "120$",
         link: "https://cdn.mos.cms.futurecdn.net/ytKr6L3ZTP4Pf2acEpzpMM.jpeg"
     }
