84ceb5f55e3b382449fcc3ed4c58c914bc5277d0
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-18 23:26:21 +0330

    DUMMY_DATA.jsx updtated compeletly

--- src/components/dummy_data.jsx
+++ src/components/dummy_data.jsx
@@ -1,44 +1,49 @@
 const DUMMY_DATA = [
     {
         id: 1,
-        model : "iphone 11 pro",
+        model : "huawei nova 9",
         cost: "500",
-        link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo_DxbDLGl62hYIt4HsjpS0KOqVc2UDErHMw&usqp=CAU"
+        link: "https://www.zdnet.com/a/img/resize/06119597d8fde27e3074dc3bb4a9ce0f1851280a/2023/04/24/4e586f53-afa2-452d-baf4-cc7c78c2c5fb/samsung-galaxy-a54-5g.jpg?auto=webp&fit=crop&height=900&width=1200"
     }, {
         id: 2,
-        model : "samsung M53",
-        cost: "1280",
-        link: "https://images.samsung.com/is/image/samsung/p6pim/in/sm-m536bzbfinu/gallery/in-galaxy-m53-5g-sm-m536-421705-sm-m536bzbfinu-532181615? 650_519_PNG "
+        model : "redmi 12",
+        cost: "763",
+        link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm8UIKrSJv5R_5N_7UFLZd2PykXNgEf_fFIg&usqp=CAU"
     }, {
         id: 3,
-        model : "samsung Z flip5",
+        model : "xiaomi 12 pro",
         cost: "740",
-        link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZQTmTEoOZcidZf7vMD6owQAj8KIB-Ir4TWJaWn2TNcKNf6a3bHTFnu1UvkA4vCVCrQsQ&usqp=CAU"
+        link: "https://img.etimg.com/thumb/width-640,height-480,imgsize-35708,resizemode-75,msid-98897778/top-trending-products/mobile-phones/6-latest-mobile-phones-with-12gb-ram-in-india-in-2023-starting-at-rs-29999/6-latest-mobile-phones-with-12gb-ram-in-india.jpg"
     }, {
         id: 4,
         model : "samsung S23",
         cost: "628",
-        link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmiAKtiWFbFuOGFav4liY881ikLKeHK96rlw&usqp=CAU"
+        link: "https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1677055988557/a3fe0a6675fede93f198a680274b06f7.png"
     }, {
         id: 5,
         model : "surface pro 10",
         cost: "3282",
-        link: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWBrzy?ver=85d4&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true"
+        link: "https://img.freepik.com/premium-psd/close-up-floating-laptop-mockup_308775-6.jpg"
     }, {
         id: 6,
-        model : "HP Envy",
-        cost: "60",
-        link: "https://cdn.thewirecutter.com/wp-content/media/2023/06/laptops-2048px-5607.jpg?auto=webp&quality=75&crop=1.91:1&width=1200"
+        model : "pro display XDR",
+        cost: "4999",
+        link: "https://img.freepik.com/free-psd/computer-screen-white-background-mock-up_1022-135.jpg"
     }, {
         id: 7,
-        model : "surfac pro 9",
-        cost: "5",
-        link: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4OXzi?ver=3a58"
+        model : "surface pro 9",
+        cost: "2499",
+        link: "https://content1.rozetka.com.ua/goods/images/big/249012572.jpg"
     }, {
         id: 8,
         model : "macbook pro",
-        cost: "120",
-        link: "https://cdn.mos.cms.futurecdn.net/ytKr6L3ZTP4Pf2acEpzpMM.jpeg"
+        cost: "3099",
+        link: "https://frame.work/assets/navigation/framework_laptop_thirteen-627beafab235555e8592dbc3b2092f5f437e5015b2af053f37fdc41162730088.png"
+    }, {
+        id: 9,
+        model : "macbook pro 2",
+        cost: "1200",
+        link: "https://m.media-amazon.com/images/I/718ETwvLVOL._SL1500_.jpg"
     }
 ]
 
