7041e8028b6b8ec73b6664ce5e342a124055957f
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-18 23:37:52 +0330

    compelete the Dynamic content for CounterContent.jsx

--- src/components/counterContent.jsx
+++ src/components/counterContent.jsx
@@ -1,56 +1,55 @@
 import { useState } from "react";
 
 const CounterContent = ({price , imgLinks , model}) => {
-    const [counter , setCounter] = useState(0)
+    const [counter , setCounter] = useState(1)
 
     const btnReducerHandler = () => {
-        if(counter <= 0) {
-            setCounter(0)
-        } else{
-            setCounter(counter - 1)
-        }
+        setCounter(counter - 1)
     }
 
     const btnIncreaseHandler = () => {
-        if(counter >= 5) {
-            setCounter(1)
+        setCounter(counter + 1)
+    }
 
-        } else{
-            setCounter(counter + 1)
-        }
+    const inputChangeHandler = (ev) => {
+        ev.target.value === "" ? setCounter(1) : setCounter(ev.target.value);
     }
 
     return (
         <>
-            <div className="w-80 m-2 overflow-hidden rounded-2xl">
+            <div className = "w-80 overflow-hidden rounded-2xl m-5 mx-8">
                 <div>
                     <img src = {imgLinks} alt="mohammad sadra nafe" className = "w-full h-full" />
                 </div>
 
-                <div className="bg-blue-500 p-3">
+                <div className = "bg-zinc-800 text-neutral-50 p-3">
                     <div className = "flex items-center flex-wrap justify-between">
                         <p>{model}</p>
-                        <p className="w-fit px-3 rounded-lg bg-orange-300">
-                            {price}
-                            <span className = {counter !== 0 ? "inline-block" : "hidden"}>
-                                <i className='bx bx-x'></i>
-                                <span>{`${counter} = ${Number(price) * counter} $`}</span>
+                        <p  className = "w-fit px-3 py-1 rounded-lg bg-zinc-950">
+                            <span className = {"inline-block"}>
+                                <span>{ counter === 0 ? Number(price).toLocaleString() : Number(Number(price) * counter).toLocaleString()} $</span>
                             </span>
                         </p>
                     </div>
 
-                    <hr className="w-full my-2 mt-3" />
+                    <hr  className = "w-full my-2 mt-3" />
 
-                    <div className="text-2xl flex flex-wrap items-center">
-                        <button className="text-2xl m-1 p-1 pb-0 disabled:text-slate-300" onClick = {btnReducerHandler} >
-                            <i className="bx bx-left-arrow-circle"></i>
-                        </button>
+                    <div  className = "flex flex-wrap items-center justify-between w-full">
+                        <div className = "w-6/12 flex justify-center items-center">
+                            <button  className = "text-2xl m-1 p-1 pb-0 disabled:text-slate-600 disabled:cursor-not-allowed" onClick = {btnReducerHandler} disabled = {counter <= 1 ? true : false}>
+                                <i  className = "bx bx-left-arrow-circle"></i>
+                            </button>
 
-                        <input type = "number" />
+                            <input type = "number" className = "text-black text-lg border px-2 bg-slate-100 rounded-lg w-4/12 outline-none" onChange = {inputChangeHandler} value = {counter <= 5 && counter >= 1 ? counter : setCounter(Number(counter - 5))}/>
 
-                        <button className="text-2xl m-1 p-1 pb-0" onClick = {btnIncreaseHandler}>
-                            <i className="bx bx-right-arrow-circle"></i>
-                        </button>
+                            <button  className = "text-2xl m-1 p-1 pb-0 disabled:text-slate-600 disabled:cursor-not-allowed" disabled = {counter >= 5 ? true : false} onClick = {btnIncreaseHandler}>
+                                <i  className = "bx bx-right-arrow-circle"></i>
+                            </button>
+                        </div>
+
+                        <div>
+                            <button className = "capitalize bg-blue-600 hover:bg-blue-800 transition-all p-2 px-5 rounded-lg">buy</button>
+                        </div>
                     </div>
                 </div>
             </div>
