2c9869c27f08a3f9fcef052a12d2fab9039fbbd0
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-18 23:46:14 +0330

    because the structure of the NavBar already wrote , just copy and add to cartCounter.jsx

---
+++ src/components/navBar/nav.jsx
@@ -0,0 +1,67 @@
+import NavLogo from "./navLogo";
+import { useState } from "react";
+
+const Nav = () => {
+    const [menuIsOpen , setMenuIsOpen] = useState(false)
+
+    const menuHandler = () => {
+        setMenuIsOpen(!menuIsOpen)    
+    }
+    
+    return (
+        <>
+            <div className = 'flex flex-wrap justify-between items-center bg-neutral-900 p-2 px-4 w-full'>
+
+                <div className = 'flex flex-wrap w-6/12 min-[930px]:flex-nowrap items-center justify-start md:w-2/12 min-[930px]:w-1/12'>
+                    
+                    <button onClick = {menuHandler} className = 'block md:hidden cursor-pointer mr-3 p-2 transition-all'>
+                        <i className = { menuIsOpen ? 'w-6 h-0.5 my-2 rotate-45 -translate-y-0 transition-all block rounded-sm bg-white' : "w-6 h-0.5 my-2 transition-all block rounded-sm bg-white"}></i>
+                        <i className = { menuIsOpen ? 'w-6 h-0.5 my-2 -rotate-45 -translate-y-2.5 transition-all block rounded-sm bg-white' : "w-6 h-0.5 my-2 transition-all block rounded-sm bg-white"}></i>
+                    </button>
+
+                    <NavLogo />
+                    <p className = 'uppercase text-white max-[929px]:text-sm font-semibold'>acme</p>
+                </div>
+
+                <div className = 'hidden capitalize md:flex md:flex-wrap justify-center items-center text-white max-[929px]:w-8/12 min-[930px]:w-10/12'>
+                    <div className = 'mx-2'>
+                        <a href = "#" className = 'p-3 px-5 hover:text-blue-200 transition-all'>features</a>
+                    </div>
+
+                    <div className = 'mx-2'>
+                        <a href = "#" className = 'p-3 px-5 hover:text-blue-200 transition-all'>customers</a>
+                    </div>
+
+                    <div className = 'mx-2'>
+                        <a href = "#" className = 'p-3 px-5 hover:text-blue-200 transition-all'>integrations</a>
+                    </div>
+                </div>
+
+
+                <div className = 'capitalize min-[930px]:w-1/12 w-4/12 md:w-2/12 max-[929px]:text-sm flex flex-wrap justify-end items-center'>
+                    <a href = "#" className = 'bg-transparent border-blue-200 text-blue-200 border hover:bg-blue-200 transition-all hover:text-black p-2 rounded-xl'>sign up</a>
+                </div>
+
+                <div className = {menuIsOpen ? 'md:hidden capitalize flex max-h-48 overflow-hidden flex-wrap justify-center items-center text-white transition-all w-full' : 'md:hidden capitalize flex max-h-0 overflow-hidden flex-wrap justify-center items-center text-white transition-all w-full'}>
+                    <div className = 'mx-2 w-full my-2 p-1'>
+                        <a href = "#" className = 'p-3 px-5 hover:text-blue-200 transition-all'>features</a>
+                    </div>
+
+                    <div className = 'mx-2 w-full my-2 p-1'>
+                        <a href = "#" className = 'p-3 px-5 hover:text-blue-200 transition-all'>customers</a>
+                    </div>
+
+                    <div className = 'mx-2 w-full my-2 p-1'>
+                        <a href = "#" className = 'p-3 px-5 hover:text-blue-200 transition-all'>integrations</a>
+                    </div>
+
+                    <div className = 'mx-2 w-full my-2 p-1'>
+                        <a href = "#" className = 'p-3 px-5 hover:text-red-600 text-red-500 transition-all'>log out</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Nav;
