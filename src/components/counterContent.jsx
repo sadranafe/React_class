7aa6965aedf9eea53e43b0a2f497b9a17b7ad9b3
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-18 15:32:24 +0330

    made the content of the counterContent.jsx Dynamic

--- src/components/counterContent.jsx
+++ src/components/counterContent.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 const CounterContent = ({price , imgLinks , model}) => {
+    const [counter , setCounter] = useState(0)
+
+    const btnReducerHandler = () => {
+        if(counter <= 0) {
+            setCounter(0)
+        } else{
+            setCounter(counter - 1)
+        }
+    }
+
+    const btnIncreaseHandler = () => {
+        if(counter >= 5) {
+            setCounter(1)
+
+        } else{
+            setCounter(counter + 1)
+        }
+    }
+
     return (
         <>
             <div className="w-80 m-2 overflow-hidden rounded-2xl">
@@ -10,8 +31,10 @@ const CounterContent = ({price , imgLinks , model}) => {
                     <div className = "flex items-center flex-wrap justify-between">
                         <p>{model}</p>
                         <p className="w-fit px-3 rounded-lg bg-orange-300">
-                            <span className = "inline-block">
+                            {price}
+                            <span className = {counter !== 0 ? "inline-block" : "hidden"}>
                                 <i className='bx bx-x'></i>
+                                <span>{`${counter} = ${Number(price) * counter} $`}</span>
                             </span>
                         </p>
                     </div>
@@ -19,11 +42,13 @@ const CounterContent = ({price , imgLinks , model}) => {
                     <hr className="w-full my-2 mt-3" />
 
                     <div className="text-2xl flex flex-wrap items-center">
-                        <button className="text-2xl m-1 p-1 pb-0 disabled:text-slate-300" >
+                        <button className="text-2xl m-1 p-1 pb-0 disabled:text-slate-300" onClick = {btnReducerHandler} >
                             <i className="bx bx-left-arrow-circle"></i>
                         </button>
 
-                        <button className="text-2xl m-1 p-1 pb-0">
+                        <input type = "number" />
+
+                        <button className="text-2xl m-1 p-1 pb-0" onClick = {btnIncreaseHandler}>
                             <i className="bx bx-right-arrow-circle"></i>
                         </button>
                     </div>
