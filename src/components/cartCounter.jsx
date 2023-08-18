eae9c9a61b860ceb2a01d0c82acaaef1261975fb
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-18 23:29:30 +0330

    div tag added to cartCounter.jsx and mapping over the DUMMY_DATA.jsx in that div.

--- src/components/cartCounter.jsx
+++ src/components/cartCounter.jsx
@@ -5,9 +5,9 @@ const CartCounter = () => {
 
     return (
         <>
-            <div className = "w-full flex flex-wrap justify-center items-center content-center">
+            <div className = "w-full flex flex-wrap bg-slate-700 justify-center items-center content-center p-5">
                 {
-                    DUMMY_DATA.map(item =>
+                    DUMMY_DATA.map( item =>
                         <CounterContent key = {item.id} price = {item.cost} imgLinks = {item.link} model = {item.model}/>
                     )
                 }
