73481a164f0903069ccb041799f2ddd80fb2e18f
mohammad sadra nafe <sadranafe7@gmail.com>
2023-09-07 14:07:41 +0330

    navBar deleted from cartCounter.jsx and imported in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Nav from "./components/navBar/nav"
 import CartCounter from "./components/cartCounter";
 
 const App = () => {
   return (
     <>
+      <Nav/>
       <CartCounter/>
     </>
   );
--- src/components/cartCounter.jsx
+++ src/components/cartCounter.jsx
@@ -1,13 +1,11 @@
 import DUMMY_DATA from "./dummy_data";
 import CounterContent from "./counterContent";
-import Nav from "./navBar/nav";
 import Footer from "./footer/footer";
 
 const CartCounter = () => {
 
     return (
         <>
-            <Nav/>
             <div className = "w-full flex flex-wrap bg-slate-700 justify-center items-center content-center p-5">
                 {
                     DUMMY_DATA.map( item =>
